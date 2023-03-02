9e6e4dace8312e13b6c449cf234fe3bde1602b4d
lukasz kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2023-03-02 17:55:26 +0100

    added missing files and folders

---
+++ src/api/onboarding-text/routes/onboarding-text.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * onboarding-text router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::onboarding-text.onboarding-text');
---
+++ src/api/onboarding-text/services/onboarding-text.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * onboarding-text service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::onboarding-text.onboarding-text');
